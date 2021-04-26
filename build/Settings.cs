8ddda824688912a196fc3c78bf8280984ee9b4e7
Brett Samblanet <brettsam@microsoft.com>
2021-04-26 08:21:25 -0700

    template

--- build/Settings.cs
+++ build/Settings.cs
@@ -23,7 +23,7 @@ namespace Build
         public const string DotnetIsolatedItemTemplatesVersion = "3.1.1733";
         public const string DotnetIsolatedProjectTemplatesVersion = "3.1.1733";
         public const string DotnetItemTemplatesVersion = "3.1.1648";
-        public const string DotnetProjectTemplatesVersion = "3.1.1648";
+        public const string DotnetProjectTemplatesVersion = "4.0.1793";
         public const string TemplateJsonVersion = "3.1.1648";
 
         public static readonly string SrcProjectPath = Path.GetFullPath("../src/Azure.Functions.Cli/");
